acc2cb4e864bdc9afc001ef6c84bf47bff6952db
Rabik <rabikthapa00@gmail.com>
2025-02-21 18:10:04 +0545

    Selected Topic for Day 6

---
+++ Day6/List_Comprehension.py
@@ -0,0 +1,3 @@
+# List comprehension = A concise(brief but comprehensive) way to create lists in Python
+#                      Compact and easier to read than traditional loops
+#                      [expression for value in iterable if condition]
