1914aa9497e63f248fc5bb30bc98808d8307045b
Rabik <rabikthapa00@gmail.com>
2025-02-27 17:35:10 +0545

    Picked Topics for day 8

--- Day8/Class_variables.py
+++ Day8/Class_variables.py
@@ -1,3 +1,4 @@
 # class variables = Shared among all instances of a class
 #                   Defined outside the constructor
-#                   Al
+#                   Allow you to share data among all objects created from that class
+
