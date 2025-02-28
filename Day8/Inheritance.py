d25ef807075c9ff3f0104c711f17b47fd812a9f0
Rabik <rabikthapa00@gmail.com>
2025-02-28 11:36:50 +0545

    Inheritance Intro

--- Day8/Inheritance.py
+++ Day8/Inheritance.py
@@ -1,3 +1,38 @@
 # Inheritance = Allows a class to inherit attributes and methods from another class
 #               Helps with code reusability and extensibility
-#               class Child(Parent)
+#               class Child(Parent)  #just lika a child inheriting attributes from a paent
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+        
+    def eat(self):
+        print(f"{self.name} is eating ")
+        
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+        
+        
+class Dog(Animal):  #Inheritance gareko even tho yo dog empty cha 
+    pass
+
+class cat(Animal):   # khali navaye pani afnai pani huna sakcha
+    def speak(self):
+        print("MEOW")
+
+class Mouse(Animal):
+    def speak(self):
+        print("mew")
+
+dog = Dog("Scooby")
+cat = cat("gato")
+mouse = Mouse("Mickey")
+
+print(dog.name)
+print(dog.is_alive)  #Esari inherit garako
+
+mouse.eat()
+mouse.sleep()   #esari vitra ko objects pani access garna milcha
+
+mouse.speak()
---
+++ Day8/Multiple_inheritance.py
@@ -0,0 +1,6 @@
+# Multiple Inheritance = inherit from more than one parent class
+#                        C(A,B)
+
+# Multi level inheritance = inherit from a parent which inherits from another parent
+#                           C(B) <- B(A) <- A
+
